e4eb7d6fbe892202a78b7a3d90d2bedfde71f4a6
ZuhChar <zcrimmel@uwyo.edu>
2022-04-21 14:19:32 -0600

    l

--- wyshell.c
+++ wyshell.c
@@ -46,7 +46,7 @@ int main()
     while (1)
     {
         printf("$> ");
-        prtn = fgets(buff, stdin);
+        prtn = fgets(buff, 4096, stdin);
         Node *Head, *current = NULL;
         Word *commands = NULL;
         rtn = parse_line(buff);
