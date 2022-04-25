3e61ed22ddf39f7fd60e4b3af36c0aea182bfb30
ZuhChar <zcrimmel@uwyo.edu>
2022-04-24 23:40:07 -0600

    l

--- wyshell.c
+++ wyshell.c
@@ -84,28 +84,28 @@ int main()
                 if (current->command == NULL)
                 {
                     current->command = strdup(lexeme);
-                    printf("%s\n", current->command);
+                    printf(":--: %s\n", current->command);
                     // printf("String duplicated");
                 }
                 else
                 {
                     addToList(lexeme, current);
-                    printf("%s\n", current->command);
+                    printf(" --: %s\n", current->command);
                     // printf("Command added to list");
                 }
                 count++;
                 // commands = head;
             }
         }
-        // commands = calloc(1, sizeof(Word));
-        // commands = current->arg_list;
-        // while(commands != NULL){
-        //     commands = commands->prev;
-        // }
-        // while (commands != NULL)
-        // {
-        //     printf("%s\n", commands->string);
-        //     commands = commands->next;
-        // }
+        commands = calloc(1, sizeof(Word));
+        commands = current->arg_list;
+        while(commands != NULL){
+            commands = commands->prev;
+        }
+        while (commands != NULL)
+        {
+            printf("%s\n", commands->string);
+            commands = commands->next;
+        }
     }
 }
